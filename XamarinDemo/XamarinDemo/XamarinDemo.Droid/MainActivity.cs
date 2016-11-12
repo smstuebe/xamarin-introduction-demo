c36f61bcad19a9ae61b2f427b5fea9dda21fcf52
Sven-Michael Stübe <stuebe2k7@gmail.com>
2016-11-12 23:24:32 +0100

    updated

--- XamarinDemo/XamarinDemo/XamarinDemo.Droid/MainActivity.cs
+++ XamarinDemo/XamarinDemo/XamarinDemo.Droid/MainActivity.cs
@@ -1,11 +1,7 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Plugin.Permissions;
 
 namespace XamarinDemo.Droid
 {
@@ -21,6 +17,11 @@ namespace XamarinDemo.Droid
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
 }
